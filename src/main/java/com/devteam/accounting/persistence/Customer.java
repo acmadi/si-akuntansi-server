ee1f07910a98520b0ad2569533910e29b62aed2b
pancara <pancara.k.wijonarko@gmail.com>
2013-12-11 07:31:52 +0700

    init web project

---
+++ src/main/java/com/devteam/accounting/persistence/Customer.java
@@ -0,0 +1,37 @@
+package com.devteam.accounting.persistence;
+
+import java.sql.Timestamp;
+
+public class Customer {
+
+    private Long id;
+    private Timestamp version;
+    private String name;
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getVersion() {
+        return version;
+    }
+
+    public void setVersion(Timestamp version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
