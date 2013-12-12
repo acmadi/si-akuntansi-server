07eb585f63a4da2bc64341caa4fd87e4ae6e5f8d
pancara <pancara.k.wijonarko@gmail.com>
2013-12-12 08:43:22 +0700

    add maven dependency

---
+++ src/main/java/com/devteam/accounting/persistence/Account.java
@@ -0,0 +1,11 @@
+package com.devteam.accounting.persistence;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pancara
+ * Date: 12/12/13
+ * Time: 8:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Account {
+}
