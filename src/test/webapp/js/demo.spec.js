1f63f5728eab2feadf976396876fb0c939adc314
pancara <pancara.k.wijonarko@gmail.com>
2014-01-02 23:21:05 +0700

    merge with client module

---
+++ src/test/webapp/js/demo.spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+describe('function add', function () {
+    it('should equals 200', function () {
+        var a = 100;
+        var b = 100;
+        expect(add(a, b)).toBe(200);
+    });
+});
